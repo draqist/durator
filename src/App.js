50405180be7a8d2eeff310176e078db6b96bc683
Draqode <Boomjay008@gmail.com>
2022-01-16 19:08:52 +0100

    about to start work on the durator, must finish this tonight

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
+
 
 function App() {
-  async function getInfo(url) {
+  const [res, setRes] = useState('')
+  let data = {url: 'https://pagalnew.com/mp3-songs/bollywood-mp3-songs/humko-humise-chura-lo-mohabbatein-128-kbps-sound.mp3'}
+  useEffect(() => {
+    async function getInfo(data) {
     const info = await fetch('https://metaworker.herokuapp.com/convert', {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify(irl)
+      'method': 'POST',
+      'headers': {
+        'Content-Type': 'application/json'
+      },
+      'body': JSON.stringify(data)
     })
-    console.log(info)
+      .then(res => {
+        console.log(res.json())
+        setRes(res.uuid)
+      })
+      .catch(err => console.log(err.message))
     }
+    getInfo(data)
+  }, [data])
   return (
     <div className="App">
-      
+        <h1>{res}</h1>
     </div>
   );
 }
