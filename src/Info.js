25923f5106ff4e4da4fd044f206d109f40b764fc
Draqode <Boomjay008@gmail.com>
2022-01-17 02:26:57 +0100

    forgot other info

--- src/Info.js
+++ src/Info.js
@@ -5,11 +5,11 @@ const Infopage = ({format,bit_rate,duration,size,start_time,nb_streams }) => {
   return (
     <div className = 'info'>
       <h4>Format: { format }</h4>
-      {/* <h4>Bit Rate: { bit_rate }</h4>
+      <h4>Bit Rate: { bit_rate }</h4>
       <h4>Duration in s: { duration }</h4>
       <h4>Size in Kb: { size }</h4>
       <h4>Start Time: { start_time }</h4>
-      <h4>Number of Streams: { nb_streams }</h4> */}
+      <h4>Number of Streams: { nb_streams }</h4>
     </div>
   )
 }
