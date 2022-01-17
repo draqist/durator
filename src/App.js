a73b9a06a2b5c1008e9a38c517288c2b27dc959a
Draqode <Boomjay008@gmail.com>
2022-01-17 02:48:33 +0100

    tweak

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
   }
   return (
     <div className="App">
-      <h1>D.u.r.a.t.o.r</h1>
+      <div className = 'h1'>D.u.r.a.t.o.r</div>
       <div className = 'search' >
         <input type='search' placeholder='Paste your URL here' value = {value} onChange = {(e)=> setValue(e.target.value)} />
         <button onClick = {() => getInfo(value)} >
