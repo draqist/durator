50685f9b022cf18ef60a0300de18bb09e350a0ef
Draqode <Boomjay008@gmail.com>
2022-01-17 02:22:18 +0100

    finished ui and functions, api misfiring

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
 import './App.css';
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
+import axios from 'axios'
+import Infopage from './Info';
 
 
 function App() {
-  const [res, setRes] = useState('')
-  let data = {url: 'https://pagalnew.com/mp3-songs/bollywood-mp3-songs/humko-humise-chura-lo-mohabbatein-128-kbps-sound.mp3'}
-  useEffect(() => {
-    async function getInfo(data) {
-    const info = await fetch('https://metaworker.herokuapp.com/convert', {
-      'method': 'POST',
-      'headers': {
-        'Content-Type': 'application/json'
-      },
-      'body': JSON.stringify(data)
-    })
-      .then(res => {
-        console.log(res.json())
-        setRes(res.uuid)
-      })
-      .catch(err => console.log(err.message))
-    }
-    getInfo(data)
-  }, [data])
+  const [value, setValue] = useState('')
+  const [result, setResult] = useState()
+  const [check, setCheck] = useState(false)
+
+  async function getInfo(value) {
+    let data = {url: value, type: 'meta'}
+    const info = await axios.post('https://metaworker.herokuapp.com/convert', data)
+    const newData = info.data
+    const ndata = newData.data
+    const fdata = ndata.uuid 
+    const mainData = await axios.get(`https://metaworker.herokuapp.com/fetch/${fdata}`)
+    const preqData = mainData.data
+    const Datamains = preqData.data
+    const Data = Datamains.result
+    const finalData = Data.format
+    setResult(finalData)
+    setCheck(true)
+  }
   return (
     <div className="App">
-        <h1>{res}</h1>
+      <h1>D.u.r.a.t.o.r</h1>
+      <div className = 'search' >
+        <input type='search' placeholder='Paste your URL here' value = {value} onChange = {(e)=> setValue(e.target.value)} />
+        <button onClick = {() => getInfo(value)} >
+          Generate
+        </button>
+      </div>
+      {
+        check && <Infopage format = {result.format_name} bit_rate = {result.bit_rate} duration = {result.duration} size = {result.size} start_time = {result.start_time} nb_streams = {result.nb_streams}   />
+      }
+      <div className = 'appInfo'>
+        <span> This app is a Golang based file metadata service that fetches the metadata/details of any media file with just its URL. </span>
+      </div>
     </div>
   );
 }
